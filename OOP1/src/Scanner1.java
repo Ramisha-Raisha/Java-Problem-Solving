e43d0d2e0fc1f1509cf847e01670c935fdfb4db9
Ramisha-Raisha <raisharamishamustafa@gmail.com>
2022-10-05 13:06:40 +0600

    Taking Input

---
+++ OOP1/src/Scanner1.java
@@ -0,0 +1,21 @@
+//TAKING INPUT 
+import java.lang.*;
+import java.util.Scanner;
+public class Scanner1{
+	public static void main(String[]args){
+	Scanner sc = new Scanner(System.in);
+	Scanner sc1 = new Scanner(System.in);
+	int num=sc.nextInt();
+    float fnum=sc.nextFloat();
+	double dnum=sc.nextDouble();
+	byte bnum=sc.nextByte();
+	String name=sc1.nextLine();
+	//System.out.println(name);
+	System.out.println(num);
+	System.out.println(fnum);
+	System.out.println(dnum);
+	System.out.println(bnum);
+	System.out.println(name);
+	
+	
+} }
